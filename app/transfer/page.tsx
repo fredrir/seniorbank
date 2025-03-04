36713a2070ea9b2186541bbebfc80f9c99ce57a0
Gina Dalene <ginada@stud.ntnu.no>
2025-03-04 16:34:15 +0100

    Create transfer page outline

---
+++ app/transfer/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+return (
+	<section>
+		<h1 className="text-5xl font-bold mt-16 mb-8 text-seniorBankDarkBlue">
+			Overfør
+		</h1>
+		<div>
+
+		</div>
+	</section>
+);
+}
