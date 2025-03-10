62698bbf61a0289aa9c1580af317a612c36f3ba0
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-03-10 12:32:03 +0100

    refactor warning section styling

--- components/homepage/WarningSection.tsx
+++ components/homepage/WarningSection.tsx
@@ -2,26 +2,28 @@ import { ShieldAlert } from "lucide-react";
 
 export function WarningSection() {
   return (
-    <div className="text-seniorbankWhite flex justify-end h-[500px] relative flex-col py-8 px-64">
+    <div className="text-seniorbankWhite flex justify-center flex-col pt-8 relative">
       <svg
         viewBox="0 0 100 100"
         preserveAspectRatio="none"
         width={100}
         height={100}
-        className="absolute top-0 left-0 w-full h-[500px] z-[-1] text-[#015aa4]"
+        className="w-full h-[200px] md:h-[500px] z-[-1] text-[#015aa4]"
       >
         <path d="M0 100 L0 50 Q50 0 100 50 L100 100" fill="currentColor" />
       </svg>
-      <div className="flex gap-4 px-12 py-36">
-        <div className="flex flex-col justify-center">
-          <ShieldAlert className="size-24" />
-        </div>
-        <div className="flex flex-col justify-center">
-          <p className="text-seniorbankWhite font-bold mt-2 text-xl">
-            Husk: Banken eller politiet vil aldri be deg om å overføre penger
-            til en ukjent konto. Hvis du er usikker, ta kontakt med banken din
-            eller din trygghetskontakt
-          </p>
+      <div className="flex justify-center absolute w-full mt-16">
+        <div className="gap-4 px-12 mx-auto max-w-4xl flex">
+          <div className="flex flex-col justify-center">
+            <ShieldAlert className="size-16 md:size-24" />
+          </div>
+          <div className="flex flex-col align-middle">
+            <p className="text-seniorbankWhite font-bold mt-2 md:text-xl">
+              Husk: Banken eller politiet vil aldri be deg om å overføre penger
+              til en ukjent konto. Hvis du er usikker, ta kontakt med banken din
+              eller din trygghetskontakt
+            </p>
+          </div>
         </div>
       </div>
     </div>
