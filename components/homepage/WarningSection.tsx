af1aa1d5a50c7f64f0a6d50e05f27c4d949eb903
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-03-17 11:05:43 +0100

    Implement shared background graphic component

--- components/homepage/WarningSection.tsx
+++ components/homepage/WarningSection.tsx
@@ -1,18 +1,12 @@
 import { ShieldAlert } from "lucide-react";
+import { BackgroundGraphic } from "../ui/BackgroundGraphic";
 
 export function WarningSection() {
   return (
-    <div className="text-seniorbankWhite flex justify-center flex-col pt-8 relative">
-      <svg
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-        width={100}
-        height={100}
-        className="w-full h-[200px] md:h-[500px] z-[-1] text-[#015aa4]"
-      >
-        <path d="M0 100 L0 50 Q50 0 100 50 L100 100" fill="currentColor" />
-      </svg>
-      <div className="flex justify-center absolute w-full mt-16">
+    <div className="text-seniorbankWhite flex justify-center flex-col pt-48">
+      <BackgroundGraphic variant="bottom-halfcircle" className="text-[#015aa4]" />
+
+      <div className="flex justify-center w-full mt-16">
         <div className="gap-4 px-12 mx-auto max-w-4xl flex">
           <div className="flex flex-col justify-center">
             <ShieldAlert className="size-16 md:size-24" />
