59a400149d3046e695ce3fc3408f569598474c0d
Maria Andreassen <mariaagnetha@hotmail.com>
2025-03-24 09:41:49 +0100

    feat: enhance support page layout and content for better user experience

--- app/kontaktside/page.tsx
+++ app/kontaktside/page.tsx
@@ -1,36 +1,49 @@
-import HeaderText from "@/components/all/HeaderText";
-import SubHeaderText from "@/components/all/SubHeaderText";
-import { BackgroundGraphic } from "@/components/ui/BackgroundGraphic";
-
 export default function SupportPage() {
   return (
-    <section className="text-2xl">
-      <BackgroundGraphic variant="inverse-topwave" className="text-[#015aa4]" />
-      <HeaderText title="Kontakt oss" className="" />
-      <h2 className="text-4xl font-bold">Utsatt for svindel?</h2>
-      <p>Kontakt banken umiddelbart på 915 05700 for hjelp.</p>
-      <h2 className="text-4xl font-bold">Ring banken</h2>
-      <p>Ring oss</p>
-      <p>
-        Kontakt vår kundehjelp om du trenger hjelp eller er usikker på bankID,
-        mobilbank og nettbank, kort og mer.
-      </p>
-      <p>Privat: 05700</p>
-      <p>Ring fra utland (+47)91505700</p>
-      <p>7-22 (10-18)</p>
-      <h2 className="text-4xl font-bold">Kontakt trygghetskontakt</h2>
-      <p>Ring oss</p>
-      <p>xxxxxxxx</p>
+    <section className="space-y-6 text-balance py-6 text-lg sm:text-xl">
+      <h1 className="text-4xl font-bold text-seniorBankDarkBlue sm:text-6xl">
+        Kontakt oss
+      </h1>
 
-      <h2 className="text-4xl font-bold">Ring politiet</h2>
-      <p>
-        Ring sentralbordet 02800 når du trenger å kontakte politiet om tjenester
-        eller annet, som ikke er en nødsituasjon.
-      </p>
-      <p>
-        Banken eller polititet vil aldri be deg om å overføre penger til en
-        annen konto.{" "}
-      </p>
+      <div className="space-y-6">
+        <h2 className="text-2xl font-bold sm:text-4xl">Utsatt for svindel?</h2>
+        <p>
+          Kontakt banken umiddelbart på <strong>915 05700</strong> for hjelp.
+        </p>
+        <h2 className="text-2xl font-bold sm:text-4xl">Kontakt banken</h2>
+        <p>
+          Kontakt vår kundehjelp om du trenger hjelp eller er usikker på bankID,
+          mobilbank og nettbank, kort og mer.
+        </p>
+        <ul className="space-y-2">
+          <li>
+            Privat: <strong>915 05700</strong>
+          </li>
+          <li>
+            Fra utland: <strong>(+47) 915 05700</strong>
+          </li>
+          <li>
+            Åpningstider: <strong>7-22 (10-18)</strong>
+          </li>
+        </ul>
+
+        <h2 className="text-2xl font-bold sm:text-4xl">
+          Kontakt trygghetskontakt
+        </h2>
+        <p>
+          Kontakt trygghetskontakt om du trenger hjelp eller er usikker på
+          transaksjoner i nettbanken, betalinger eller andre banktjenester.
+        </p>
+        <p>
+          Ring trygghetskontakten på <strong>xxxxxxxx</strong>
+        </p>
+
+        <h2 className="text-2xl font-bold sm:text-4xl">Politiet</h2>
+        <p>
+          Ring sentralbordet <strong>02800</strong> når du trenger å kontakte
+          politiet om tjenester eller annet, som ikke er en nødsituasjon.
+        </p>
+      </div>
     </section>
   );
 }
