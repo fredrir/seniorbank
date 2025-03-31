23654dbe28d8e439c1665110281b00ff6bf7fdef
fredrir <fhansteen@gmail.com>
2025-03-31 14:31:27 +0200

    .

--- components/contact-page/ContactNavbar.tsx
+++ components/contact-page/ContactNavbar.tsx
@@ -31,14 +31,14 @@ const ContactNavbar = () => {
   return (
     <div className="relative w-full bg-[#F8E9DD] px-4">
       <nav className="flex w-full items-center justify-center">
-        <span className="flex flex-row items-center justify-center border-b-2 border-white">
+        <span className="flex flex-row items-center justify-center">
           {navLinks.map((link, index) => (
             <Link
               href={link.path}
               key={index}
               className={`flex flex-row items-center justify-center rounded-t-2xl p-4 text-2xl transition-colors ${
                 pathname === link.path
-                  ? "border-seniorBankLightBlue bg-white text-seniorBankDarkBlue underline active:bg-seniorBankLightBlue active:text-seniorBankDarkBlue"
+                  ? "border-seniorBankLightBlue bg-seniorbankWhite text-seniorBankDarkBlue underline active:bg-seniorBankLightBlue active:text-seniorBankDarkBlue"
                   : "text-seniorBankDarkBlue hover:bg-seniorBankLightBlue hover:text-seniorBankDarkBlue active:bg-seniorBankDarkBlue active:text-seniorBankDarkBlue"
               }`}
             >
