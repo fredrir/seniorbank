bf7165d2ecaa8c27f6a68615ba273ebb3926308a
fredrir <fhansteen@gmail.com>
2025-03-31 14:24:39 +0200

    add background and new pages

---
+++ app/(contact)/kontakt/layout.tsx
@@ -0,0 +1,18 @@
+import ContactNavbar from "@/components/contact-page/ContactNavbar";
+import { BackgroundGraphic } from "@/components/ui/BackgroundGraphic";
+
+export default function UserLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <ContactNavbar />
+      <div className="mx-auto max-w-4xl px-2 md:px-0">
+        {children}{" "}
+        <BackgroundGraphic variant="mid-wave" className="text-[#F8E9DD]" />
+      </div>
+    </>
+  );
+}
--- components/ui/BackgroundGraphic.tsx
+++ components/ui/BackgroundGraphic.tsx
@@ -4,7 +4,8 @@ type GraphicVariant =
   | "top-halfcircle"
   | "bottom-halfcircle"
   | "topwave"
-  | "inverse-topwave";
+  | "inverse-topwave"
+  | "mid-wave";
 
 export function BackgroundGraphic({
   variant,
@@ -58,6 +59,22 @@ export function BackgroundGraphic({
         </svg>
       );
     case "inverse-topwave":
+      return (
+        <svg
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+          width={100}
+          height={100}
+          className={cn("absolute left-0 z-[-1] h-[500px] w-full", className)}
+        >
+          <path
+            d="M0 0 L0 75 C20 65, 50 70, 70 90 S100 95, 100 90 L100 0 Z"
+            fill="currentColor"
+          />
+        </svg>
+      );
+
+    case "mid-wave":
       return (
         <svg
           viewBox="0 0 100 100"
