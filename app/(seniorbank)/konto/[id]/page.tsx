e8764533eb978230b658ca6870a0fd244c4669cf
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-04-07 17:01:51 +0200

    Reorganize account page styling

--- app/(seniorbank)/konto/[id]/page.tsx
+++ app/(seniorbank)/konto/[id]/page.tsx
@@ -44,46 +44,40 @@ export default async function AccountPage(props: { params: tParams }) {
 
   return (
     <>
-      <section>
-        <BackgroundGraphic
-          variant="top-halfcircle"
-          className="text-[#015aa4] h-[800px]"
-        />
-        <div className="max-w-fit">
-          <Heading title={account.name} href="/konto">
-            <p className="text-end text-3xl text-seniorBankLightBlue mr-2 mt-[-10px]">
-              {decodedId}
+    <div className="max-w-fit">
+      <Heading title={account.name} href="/konto">
+        <p className="text-end text-3xl text-seniorBankLightBlue mr-2 mt-[-10px]">
+          {decodedId}
+        </p>
+      </Heading>
+    </div>
+
+    <section className="my-16 flex flex-col items-center text-white">
+      <h3 className="text-2xl font-bold">Saldo</h3>
+      <h2 className="text-4xl font-bold md:text-6xl">
+        {formatCurrency(account.balance, true)}
+      </h2>
+      <div className="mb-4 mt-16 w-full px-4">
+        <div className="relative w-full">
+          <Search className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-white/70" />
+          <Input
+            className="w-full rounded-full border-none bg-[#4D8CBF] pl-10 text-white placeholder:text-white/70"
+            placeholder="Søk"
+          />
+        </div>
+      </div>
+      {
+        transactions.length === 0 ? (
+          <div>
+            <p className="text-center text-2xl font-bold text-white">
+              Ingen transaksjoner
             </p>
-          </Heading>
-        </div>
-      </section>
-      <section className="my-16 flex flex-col items-center text-white">
-        <h3 className="text-2xl font-bold">Saldo</h3>
-        <h2 className="text-4xl font-bold md:text-6xl">
-          {formatCurrency(account.balance, true)}
-        </h2>
-        <div className="mb-4 mt-16 w-full px-4">
-          <div className="relative w-full">
-            <Search className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-white/70" />
-            <Input
-              className="w-full rounded-full border-none bg-[#4D8CBF] pl-10 text-white placeholder:text-white/70"
-              placeholder="Søk"
-            />
           </div>
-        </div>
-        {
-          transactions.length === 0 ? (
-            <div>
-              <p className="text-center text-2xl font-bold text-white">
-                Ingen transaksjoner
-              </p>
-            </div>
-          ) : (
-            <TransactionList transactions={transactions} user={user}/>
-          )
-        }
-        
-      </section>
+        ) : (
+          <TransactionList transactions={transactions} user={user}/>
+        )
+      }
+    </section>
     </>
   );
 }
