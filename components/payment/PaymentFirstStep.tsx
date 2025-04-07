094667fbf5051bcdce578093155506293653d153
Isabelle Nordin <isabelle@nordin.no>
2025-04-07 14:46:28 +0200

    Remove commented code

--- components/payment/PaymentFirstStep.tsx
+++ components/payment/PaymentFirstStep.tsx
@@ -1,7 +1,6 @@
 import { Button } from "../ui/button";
 import { ProgressBar } from "../all/ProgressBar";
 import AccountView from "./AccountView";
-//  import { useSession } from "next-auth/react";
 
 interface PaymentFirstStepProps {
   onClick: () => void;
@@ -18,10 +17,7 @@ const PaymentFirstStep = ({
   selectedAccount,
   isHard,
 }: PaymentFirstStepProps) => {
-  // const { data: session } = useSession();
 
-  //  const difficulty = session?.user?.difficulty;
-  //  console.log("User Difficulty:", difficulty);
 
   const hardProgressbar = <ProgressBar totalSteps={3} currentStep={1} />;
   const mediumProgressbar = <ProgressBar totalSteps={4} currentStep={1} />;
