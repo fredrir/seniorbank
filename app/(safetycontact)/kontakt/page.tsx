06f9d8c7c2f06bb10c16f3fc40e3c28247dab6bb
Fredrik Hansteen <fhansteen@gmail.com>
2025-04-22 21:54:45 +0200

    86 medium finish contact page (#87)
    * fix approval card
    
    * Make approval slider use ApprovalCard
    
    * add previous approvals page
    
    * add account page
    
    * fix lint shit

--- app/(safetycontact)/kontakt/page.tsx
+++ app/(safetycontact)/kontakt/page.tsx
@@ -2,15 +2,14 @@ import { checkRegisteredUser } from "@/lib/auth";
 import AccountOverview from "@/app/(safetycontact)/kontakt/(components)/AccountOverview";
 import ActionOverview from "@/app/(safetycontact)/kontakt/(components)/ActionOverview";
 import ApprovalSlider from "@/app/(safetycontact)/kontakt/(components)/ApprovalSlider";
+import Heading from "./(components)/Heading";
 
 export default async function ContactPersonPage() {
   const user = await checkRegisteredUser();
 
   return (
     <main>
-      <h1 className="pt-8 text-2xl font-bold text-[#002776] md:text-4xl">
-        Velkommen, {user.name}!
-      </h1>
+      <Heading title={`Velkom, ${user.name}!`} className="pt-8" />
 
       {/* TODO integrere med backend */}
       <h3 className="mx-auto max-w-3xl pt-4 text-lg">
