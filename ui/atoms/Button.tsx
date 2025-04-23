c159d88d2b471c184a896e8ae2f2b68c8a705c02
fredrir <fhansteen@gmail.com>
2025-04-23 11:18:15 +0200

    hotfix calendar modal

--- ui/atoms/Button.tsx
+++ ui/atoms/Button.tsx
@@ -8,24 +8,26 @@ import { ChevronRight } from "lucide-react";
 
 const buttonVariants = cva(
   cn(
-    "inline-flex relative items-center justify-center whitespace-nowrap rounded-xl text-sm font-medium transition-colors",
+    "inline-flex relative items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors",
     "focus-visible:ring-1 focus-visible:ring-ring",
     "disabled:pointer-events-none disabled:opacity-50",
-    "[&_svg]:pointer-events-none font-extrabold",
+    "[&_svg]:pointer-events-none",
   ),
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        default: "bg-[#002776] text-white shadow hover:bg-[#002776]/90",
         destructive:
-          "bg-destructive text-destructive-foreground outline-primary outline-4 shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground outline-[#002776] outline-2 shadow-sm hover:bg-destructive/90",
+        outline:
+          "border border-[#002776] bg-[#FDF8F5] text-[#002776] shadow-sm hover:bg-[#F8E9DD]",
+        ghost: "bg-transparent text-[#002776] hover:bg-[#F8E9DD]",
       },
       size: {
-        default: "h-9 px-16 min-w-64 py-6 text-2xl",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: "h-8 px-4 py-2 text-sm",
+        sm: "h-6 rounded-md px-2 text-xs",
+        lg: "h-9 rounded-md px-6",
+        icon: "h-5 w-5",
       },
     },
     defaultVariants: {
@@ -52,8 +54,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       >
         {props.children}
-
-        {chevron && <ChevronRight className="absolute right-4 h-96" />}
+        {chevron && <ChevronRight className="absolute right-2 h-4 w-4" />}
       </Comp>
     );
   },
